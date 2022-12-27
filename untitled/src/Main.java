8c2e4210cc44a1e2e0792ec6f00770d68d499978
GrafovaDaria <106280477+GrafovaDaria@users.noreply.github.com>
2022-12-27 13:34:37 +0300

    Add files via upload

---
+++ untitled/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        double a=5.7, b=6.4, c=3.1, x=2.8, y;
+        y=(Math.sqrt(Math.exp(a*x)+Math.pow(x,2))*Math.log(Math.pow(x, 2)+b*x+10))/(Math.sin(x*c)+4.2);
+        System.out.println(y);
+    }
+}
